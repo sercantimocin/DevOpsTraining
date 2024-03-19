64d5cdab1302fea8941166b466c311e5d89bdd2b
Sercan Timoçin <sercantimocin@gmail.com>
2024-03-19 23:09:57 +0100

    Get All Students added

---
+++ StudentAPI.UnitTests/StudentControllerTest.cs
@@ -0,0 +1,31 @@
+﻿using StudentAPI.Controllers;
+using StudentAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentAPI.UnitTests
+{
+    public class StudentControllerTest
+    {
+        [Fact]
+        public void GetAllStudentsAsync()
+        {
+            var student = new Student() { Id = 1, Name = "John" };
+
+            Dictionary<int,Student> students = new Dictionary<int,Student>();
+            students.Add(student.Id, student);
+
+            var studentController = new StudentsController(students);
+
+            var result = studentController.GetAllStudents();
+
+            Assert.NotNull(result);
+            Assert.Single(result);
+            Assert.Equal(student,result.First());
+        }
+    }
+}
---
+++ StudentAPI/Controllers/StudentsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OpenApi;
+using StudentAPI.Models;
+
+namespace StudentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private Dictionary<int, Student> students;
+
+        public StudentsController(Dictionary<int, Student> students)
+        {
+            this.students = students;
+        }
+
+        [HttpGet]
+        public List<Student> GetAllStudents()
+        {
+            return students.Values.ToList();
+        }
+    }
+}
